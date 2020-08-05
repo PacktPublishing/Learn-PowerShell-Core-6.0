eb0c1bbb675ffb1412aa356c9739b35793bf8a66
David das Neves <ddneves@users.noreply.github.com>
2020-08-05 14:55:10 +0200

    Update 01_Obfuscation.ps1
    added comments

--- Chapter07/05_Obfuscation/01_Obfuscation.ps1
+++ Chapter07/05_Obfuscation/01_Obfuscation.ps1
@@ -1,32 +1,21 @@
-﻿#Obfuscated script
+# Obfuscated script
 .("{3}{1}{2}{0}" -f 'Host', 'ri', 'te-', 'W') ("{2}{1}{0}{8}{5}{9}{6}{4}{7}{3}" -f 'hell', 'owerS', 'P', 'uage', 'tic ', 'a fan', 's', 'lang', ' is ', 'ta')
 
-#removing dot sourcing mechanism
-#first part of the script
+# removing dot sourcing mechanism
+# first part of the script
 "{3}{1}{2}{0}" -f 'Host', 'ri', 'te-', 'W' #Write-Host
 
-#last part of the script
+# last part of the script
 "{2}{1}{0}{8}{5}{9}{6}{4}{7}{3}" -f 'hell', 'owerS', 'P', 'uage', 'tic ', 'a fan', 's', 'lang', ' is ', 'ta' #"PowerShell is a fantastic language"
 
-
-#((47,65,74,'2d',53,65,72,76,69,63,65,20,42,49,54,53)|fOREach{([cONvERT]::Toint16(([sTRING]$_),16)-as[ChaR])})-jOin''|&( $eNV:coMSpec[4,24,25]-JOIn'')
-
-
-
+# The following line executes the BITS service
+# the following PowerShell line is hidden: Get-Service BITS | IEX 
+((47,65,74,'2d',53,65,72,76,69,63,65,20,42,49,54,53)|fOREach{([cONvERT]::Toint16(([sTRING]$_),16)-as[ChaR])})-jOin''|&( $eNV:coMSpec[4,24,25]-JOIn'')
 ((47, 65, 74, '2d', 53, 65, 72, 76, 69, 63, 65, 20, 42, 49, 54, 53)|fOREach {([cONvERT]::Toint16(([sTRING]$_), 16) -as [ChaR])}) -jOin ''|&( $eNV:coMSpec[4, 24, 25] -JOIn '')
 
-
-
-#inVOkE-ExpREssIOn(-joIn( '71!101x116n45&83x101n114&118V105V99V101V32&66W73V84z83'-SPLit'W' -spLiT'!'-spLiT '&'-sPliT 'J' -spLit'V' -spliT 'x'-SplIt 'n'-SpliT 't'-SpLiT'Y' -SpliT'z' | fOReAcH {([cHaR][Int] $_) } ) )
+# Get-Service BITS 
 -joIn ( '71!101x116n45&83x101n114&118V105V99V101V32&66W73V84z83' -SPLit 'W' -spLiT '!' -spLiT '&' -sPliT 'J' -spLit 'V' -spliT 'x' -SplIt 'n' -SpliT 't' -SpLiT 'Y' -SpliT 'z' | fOReAcH {([cHaR][Int] $_) } )
 
-
-#([RUntiMe.interOpsERvICes.MARshaL]::([RunTiMe.iNtErOPseRvIceS.mARShal].gETmemBerS()[4].NAmE).INvOKe([RUNTImE.inTeRoPSERvICes.mARshaL]::secUREstriNgtoGLObaLALLOcUniCoDE($('76492d1116743f0423413b16050a5345MgB8ADYAYgBIAGQAWABwAGcAbgBYAG0ARABSAFoASQBkAEgAcwAzAFgAWQAxAFEAPQA9AHwANgA2AGQAMwBiADIAOQA2ADgAOABmADkAYQA2ADMAOQAwAGQAOAA1AGIAMQA1AGQAYQAxADcAYQA2AGMAOQA1ADMAYQA0AGQAYwA3ADgAZABjAGUAMgAwAGYANwAyADAAMQA1ADcAZQAyAGEAYQBmADkAZQAxADMAOQAyADgAOAAxAGUAZQAyADEAOQBmADkAMQBkADkANgAzADAAOAA3ADQAZQA0AGIANgA0AGIAZgBkADIAMwA4ADgAYgA3ADUA'|convERTTo-SecuREsTRINg  -KEy 203,44,105,6,209,29,205,9,226,126,34,92,19,130,12,164,96,90,234,90,131,187,101,158,136,170,213,99,230,174,102,106) ) ))|IeX
-
+# Get-Service BITS 
 ([RUntiMe.interOpsERvICes.MARshaL]::([RunTiMe.iNtErOPseRvIceS.mARShal].gETmemBerS()[4].NAmE).INvOKe([RUNTImE.inTeRoPSERvICes.mARshaL]::secUREstriNgtoGLObaLALLOcUniCoDE($('76492d1116743f0423413b16050a5345MgB8ADYAYgBIAGQAWABwAGcAbgBYAG0ARABSAFoASQBkAEgAcwAzAFgAWQAxAFEAPQA9AHwANgA2AGQAMwBiADIAOQA2ADgAOABmADkAYQA2ADMAOQAwAGQAOAA1AGIAMQA1AGQAYQAxADcAYQA2AGMAOQA1ADMAYQA0AGQAYwA3ADgAZABjAGUAMgAwAGYANwAyADAAMQA1ADcAZQAyAGEAYQBmADkAZQAxADMAOQAyADgAOAAxAGUAZQAyADEAOQBmADkAMQBkADkANgAzADAAOAA3ADQAZQA0AGIANgA0AGIAZgBkADIAMwA4ADgAYgA3ADUA'|convERTTo-SecuREsTRINg  -KEy 203, 44, 105, 6, 209, 29, 205, 9, 226, 126, 34, 92, 19, 130, 12, 164, 96, 90, 234, 90, 131, 187, 101, 158, 136, 170, 213, 99, 230, 174, 102, 106) ) ))
-
-
-
-Add-MpPreference -AttackSurfaceReductionRules_Ids 5BEB7EFE-FD9A-4556-801D-275E5FFC04CC -AttackSurfaceReductionRules_Actions Enabled
-
 
